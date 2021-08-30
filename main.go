c2f71d701c02734b963ebc93c1f11d4a4889650b
MakhmudEgo <makhmudego@yandex.ru>
2021-08-30 21:57:11 +0300

    fix lifetime bug

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 )
 
 type Note struct {
-	Time time.Time
-	Text string
+	Time     time.Time
+	Text     string
 	Lifetime time.Time
 }
-
 
 func main() {
 	Database := make(map[string][]Note)
@@ -44,7 +43,8 @@ func main() {
 
 func GetFirstNote(Database map[string][]Note) {
 	http.HandleFunc("/api/get-first-note", func(w http.ResponseWriter, r *http.Request) {
-		user := r.URL.Query().Get("user"); if user == "" {
+		user := r.URL.Query().Get("user")
+		if user == "" {
 			SendMsg(w, "/api/add-note", "Empty user")
 			return
 		}
@@ -55,7 +55,7 @@ func GetFirstNote(Database map[string][]Note) {
 
 		// check lifetime
 		for i, note := range Database[user] {
-			if note.Time.After(note.Lifetime) {
+			if !note.Lifetime.IsZero() && time.Now().After(note.Lifetime) {
 				Database[user] = append(Database[user][:i], Database[user][i+1:]...)
 			}
 		}
@@ -65,7 +65,8 @@ func GetFirstNote(Database map[string][]Note) {
 			return
 		}
 
-		js, err := json.Marshal(Database[user][0]); if err != nil {
+		js, err := json.Marshal(Database[user][0])
+		if err != nil {
 			log.Fatal("get-first-note: " + err.Error())
 		}
 		SendMsg(w, "/api/get-first-note", string(js))
@@ -74,7 +75,8 @@ func GetFirstNote(Database map[string][]Note) {
 
 func GetLastNote(Database map[string][]Note) {
 	http.HandleFunc("/api/get-last-note", func(w http.ResponseWriter, r *http.Request) {
-		user := r.URL.Query().Get("user"); if user == "" {
+		user := r.URL.Query().Get("user")
+		if user == "" {
 			SendMsg(w, "/api/get-last-note", "Empty user")
 			return
 		}
@@ -85,7 +87,7 @@ func GetLastNote(Database map[string][]Note) {
 
 		// check lifetime
 		for i, note := range Database[user] {
-			if note.Time.After(note.Lifetime) {
+			if !note.Lifetime.IsZero() && time.Now().After(note.Lifetime) {
 				Database[user] = append(Database[user][:i], Database[user][i+1:]...)
 			}
 		}
@@ -95,7 +97,8 @@ func GetLastNote(Database map[string][]Note) {
 			return
 		}
 
-		js, err := json.Marshal(Database[user][len(Database[user])-1]); if err != nil {
+		js, err := json.Marshal(Database[user][len(Database[user])-1])
+		if err != nil {
 			log.Fatal("get-last-note: " + err.Error())
 		}
 		SendMsg(w, "/api/get-last-note", string(js))
@@ -104,7 +107,8 @@ func GetLastNote(Database map[string][]Note) {
 
 func GetAllNotes(Database map[string][]Note) {
 	http.HandleFunc("/api/get-all-notes", func(w http.ResponseWriter, r *http.Request) {
-		user := r.URL.Query().Get("user"); if user == "" {
+		user := r.URL.Query().Get("user")
+		if user == "" {
 			SendMsg(w, "/api/get-all-notes", "Empty user")
 			return
 		}
@@ -115,7 +119,7 @@ func GetAllNotes(Database map[string][]Note) {
 
 		// check lifetime
 		for i, note := range Database[user] {
-			if note.Time.After(note.Lifetime) {
+			if !note.Lifetime.IsZero() && time.Now().After(note.Lifetime) {
 				Database[user] = append(Database[user][:i], Database[user][i+1:]...)
 			}
 		}
@@ -125,7 +129,8 @@ func GetAllNotes(Database map[string][]Note) {
 			return
 		}
 
-		js, err := json.Marshal(Database[user]); if err != nil {
+		js, err := json.Marshal(Database[user])
+		if err != nil {
 			log.Fatal("get-all-notes: " + err.Error())
 		}
 		SendMsg(w, "/api/get-all-notes", string(js))
@@ -134,7 +139,8 @@ func GetAllNotes(Database map[string][]Note) {
 
 func DeleteNote(Database map[string][]Note) {
 	http.HandleFunc("/api/delete-note", func(w http.ResponseWriter, r *http.Request) {
-		user := r.URL.Query().Get("user"); if user == "" {
+		user := r.URL.Query().Get("user")
+		if user == "" {
 			SendMsg(w, "/api/delete-note", "Empty user")
 			return
 		}
@@ -142,7 +148,8 @@ func DeleteNote(Database map[string][]Note) {
 			SendMsg(w, "/api/delete-note", "Not found user")
 			return
 		}
-		id := r.URL.Query().Get("id"); if id == "" {
+		id := r.URL.Query().Get("id")
+		if id == "" {
 			SendMsg(w, "/api/delete-note", "Empty id note")
 			return
 		}
@@ -155,7 +162,7 @@ func DeleteNote(Database map[string][]Note) {
 
 		// ::Todo no effect
 		for i, _ := range Database[user] {
-			if i == res  {
+			if i == res {
 				Database[user] = append(Database[user][:i], Database[user][i+1:]...)
 				SendMsg(w, "/api/delete-note", "Note deleted")
 				return
@@ -169,7 +176,8 @@ func AddNewNote(Database map[string][]Note) {
 	http.HandleFunc("/api/add-note", func(w http.ResponseWriter, r *http.Request) {
 		var reply string
 		var timeHours int
-		user := r.URL.Query().Get("user"); if _, ok := Database[user]; !ok {
+		user := r.URL.Query().Get("user")
+		if _, ok := Database[user]; !ok {
 			SendMsg(w, "/api/add-note", "Not found user")
 			return
 		}
@@ -195,19 +203,25 @@ func AddNewNote(Database map[string][]Note) {
 				timeHours = t
 			}
 		}
+		var lt time.Time
+		if timeHours != 0 {
+			lt = time.Now()
+			lt.Add(time.Duration(timeHours) * time.Hour)
+		}
 		currentTime := time.Now()
 		Database[user] = append(Database[user],
 			Note{
-				Time: currentTime,
-				Text: text,
-				Lifetime: currentTime.Add(time.Duration(timeHours) * time.Hour)})
+				Time:     currentTime,
+				Text:     text,
+				Lifetime: lt})
 		reply = "Added new Note"
 		SendMsg(w, "/api/add-note", reply)
 	})
 }
 
 func SendMsg(w http.ResponseWriter, uri string, reply string) {
-	_, err := fmt.Fprintln(w, reply); if err != nil {
+	_, err := fmt.Fprintln(w, reply)
+	if err != nil {
 		log.Fatal(uri, err)
 	}
 }
@@ -222,7 +236,8 @@ func AddNewUser(Database map[string][]Note) {
 		} else {
 			reply = "Such a user already exists"
 		}
-		_, err := fmt.Fprintln(w, reply); if err != nil {
+		_, err := fmt.Fprintln(w, reply)
+		if err != nil {
 			log.Fatal("/api/add-new-user: ", err)
 		}
 	})
